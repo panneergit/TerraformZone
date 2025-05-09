549d40719da070d1a18707e8cb2ea961d8509090
Panneer <panneer.bsr@gmail.com>
2025-05-09 17:39:12 +0530

    Modified output

--- AWSCloud/Modules/eks/output.tf
+++ AWSCloud/Modules/eks/output.tf
@@ -1,9 +1,9 @@
 output "cluster_id" {
-   value       = try(aws_eks_cluster.this[0].cluster_id, "")
+   value       = try(aws_eks_cluster.this.cluster_id, "")
    description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
 }
 
 output "cluster_version" {
   description = "The Kubernetes version for the cluster"
-  value       = try(aws_eks_cluster.this[0].version, null)
+  value       = try(aws_eks_cluster.this.version, null)
 }
